c17dbddec525bc2d3ed138f0dfb2f56e52cd3be2
(lakshuu-jay) <lakshu.jk13@gmail.com>
2026-02-23 20:36:53 +0400

    RecursiveProblems2

--- BalancedParentheses.py
+++ BalancedParentheses.py
@@ -9,3 +9,11 @@ def paren(s,l,r,p,n):
         paren(s,l,r+1,p+1,n)
     if(l<n):
         s[p]="{"
+        paren(s,l+1,r,p+1,n)
+
+n=int(input("Enter number of parenthesis:"))
+s = [""]*2*n
+print("\n")
+paren(s,0,0,0,n)
+
+
