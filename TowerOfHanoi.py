5bbe0c1717d6fde525d93be03bdac7b34317d6f5
(lakshuu-jay) <lakshu.jk13@gmail.com>
2026-02-27 15:45:32 +0400

    RecursiveProblems3

--- TowerOfHanoi.py
+++ TowerOfHanoi.py
@@ -1,11 +1,11 @@
-def hanoi(n,source,helper,destination):
+def hanoi(n,a,b,c):
     if n==1:
-        print("Move disk 1 from",source,"to",destination)
+        print("Move disk 1 from",a,"to",b)
         return
     
-    hanoi(n-1,source,destination,helper)
-    print("Move disk",n,"from",source,"to",destination)
-    hanoi(n-1,helper,source,destination)
+    hanoi(n-1,a,c,b)
+    print("Move disk",n,"from",a,"to",b)
+    hanoi(n-1,c,b,a)
 
 n=4
-hanoi(n,"source","destination","helper")
+hanoi(n,"A","C","B")
