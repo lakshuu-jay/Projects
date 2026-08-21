e592b62cf86289e75305fc181eb13c0c888f3487
(lakshuu-jay) <lakshu.jk13@gmail.com>
2026-08-21 21:09:02 +0400

    BasicsOfProbability

--- PickABall.py
+++ PickABall.py
@@ -3,7 +3,7 @@ import random
 def pick_ball_experiment():
     balls = ['Red','Blue','Green']
     result=random.choice(balls)
-    pro = balls.count('Red').len(balls)
+    pro = balls.count('Red')/len(balls)
     print("Probability of Picking Red Ball is:",pro)
 
     if result=='Red':
